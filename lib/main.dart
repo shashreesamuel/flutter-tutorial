5335c7d77cfe92c079c84ad504106ec7b76f01c5
TheCoderGuru <shashreeshachindrasamuel14@gmail.com>
2022-06-23 20:55:55 -0400

    currently understanding flutter rows

---
+++ lib/main.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(
+  home: Home(),
+));
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text('Flutter Tutorial'),
+          centerTitle: true,
+          backgroundColor: Colors.red[600]
+      ),
+     
+
+     /* Understanding rows */
+
+
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'Test',
+          ),
+          Text('1')
+        ],
+      ),
+      
+
+      /* Containers and Padding */
+
+
+      // Padding(
+      //   padding: EdgeInsets.all(20),
+      //   child: Text('Hello'),
+      // ),
+      // Container(
+      //   padding: EdgeInsets.all(20),
+      //   padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      //   padding: EdgeInsets.fromLTRB(20, 10, 15, 23),
+      //   margin: EdgeInsets.all(30.0),
+      //   color: Colors.grey[400],
+      //   child: Text('Lorem Ipsum'),
+      // ),
+      // Center(
+        // child: Text(
+        //   'this is a dummy message',
+        //   style: TextStyle(
+        //     fontSize: 20.0,
+        //     fontWeight: FontWeight.bold,
+        //     letterSpacing: 2.0,
+        //     color: Colors.grey[600],
+        //     fontFamily: 'Roboto Mono',
+        //   ),
+        // ),
+
+
+        /* Working with images (Asset and Network Images) */
+
+
+        // child: Image.network('https://images.unsplash.com/photo-1598760122223-45f0f18a1bbd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')
+        // child: Image.asset('assets/picture.jpg')
+        
+        
+        /* Working with Icons and Buttons */
+
+
+        // child: Icon(
+        //   Icons.arrow_back,
+        //   color: Colors.amber,
+        //   size: 50.0,
+        //   ),
+
+        // child: ElevatedButton(
+        //   onPressed: (){},
+        //   child: const Icon(
+        //     Icons.outlet_outlined
+        //     ),
+        //   ),
+
+        // child: TextButton(
+        //   onPressed: (){
+        //     print("You clicked in something pretty awesome");
+        //   },
+        //   child: const Text(
+        //     'click here for something cool',
+        //     style: TextStyle(
+        //       backgroundColor: Colors.green, 
+        //       color: Colors.white
+        //       ),
+        //     ), 
+        //   ),
+
+      //   child: IconButton(
+      //     icon: const Icon(Icons.alternate_email), 
+      //     onPressed: (){
+      //       print("you clicked me to lead to something pretty awesome :)");
+      //     },
+      //     color: Colors.amber,
+      //     iconSize: 40,
+      //     ),
+      // ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        backgroundColor: Colors.red[600],
+        child: const Text('click'),
+      ),
+    );
+  }
+}
+
